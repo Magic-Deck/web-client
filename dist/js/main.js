1e66598ada0f8abf05d952c389d2ff163b894ea9
Terry Martin <terryzmartin@gmail.com>
2015-07-27 00:12:36 -0700

    add autolayout style for creating two col lists

--- dist/js/main.js
+++ dist/js/main.js
@@ -4,7 +4,7 @@
  * 
  * Download the Ponyshow App: http://www.semanticpress.com/ponyshow
  * 
- * JS/CSS Client v1.0.2
+ * JS/CSS Client v1.0.3
  * Full source at https://github.com/Ponyshow/ponyshow
  *
  * Author: TZ Martin, <martin@semanticpress.com>
