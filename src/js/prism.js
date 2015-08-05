3a7dfc090c798dc12dc8826d11529d4f76149f4e
Terry Martin <terryzmartin@gmail.com>
2015-08-05 13:22:41 -0700

    update Prism.js to support show-laguage plugin

--- src/js/prism.js
+++ src/js/prism.js
@@ -1361,3 +1361,28 @@ function getLanguage(tag) {
 }
 
 })();
+
+/* **********************************************
+     Begin prism-show-language.js
+********************************************** */
+
+(function(){
+
+if (!self.Prism) {
+	return;
+}
+
+var Languages = {
+	'csharp': 'C#',
+	'cpp': 'C++'
+};
+Prism.hooks.add('before-highlight', function(env) {
+	var pre = env.element.parentNode;
+	if (!pre || !/pre/i.test(pre.nodeName)) {
+		return;
+	}
+	var language = Languages[env.language] || env.language;
+	pre.setAttribute('data-language', language);
+});
+
+})();
